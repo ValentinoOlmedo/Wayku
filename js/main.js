20d7c81d78193e64f679df4ee21f9457878d67b6
valen <valentinomateoolmedo@gmail.com>
2025-05-31 21:24:40 -0300

    Desarollo de estilos catalogo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 const searchToggle = document.getElementById('searchToggle');
 const searchBar = document.getElementById('searchBar');
+const links = document.querySelectorAll('.main-nav a');
+const sections = document.querySelectorAll('section[data-section]');
+const catalogoSection = document.getElementById('catalogo'); 
+const dropdown = document.querySelector('.dropdown');
+const toggle = document.getElementById('dropdownText');
+const menu = document.getElementById('dropdownMenu');
+const selectedOption = document.querySelector('.selected-option');
+const options = menu.querySelectorAll('li');
 
+    
 
 searchToggle.addEventListener('click', () => {
   if (searchBar.style.display === 'block') {
@@ -17,3 +26,59 @@ searchToggle.addEventListener('click', () => {
   }
 });
 
+  
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    const target = link.getAttribute('href'); 
+    const id = target.replace('#', '');
+
+        
+    links.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+
+        
+    if (id === 'inicio') {
+      sections.forEach(section => {
+        if (section.id === 'catalogo') {
+          section.style.display = 'none'; 
+        } else {
+          section.style.display = ''; 
+        }
+      });
+    } else if (id === 'catalogo') {
+      sections.forEach(section => {
+        if (section.id === 'catalogo') {
+          section.style.display = ''; 
+        } else {
+          section.style.display = 'none'; 
+        }
+      });
+    }
+  });
+});
+
+    
+window.addEventListener('DOMContentLoaded', () => {
+  if (catalogoSection) {
+    catalogoSection.style.display = 'none';
+  }
+});
+
+
+toggle.addEventListener('click', () => {
+      dropdown.classList.toggle('show');
+    });
+
+options.forEach(option => {
+  option.addEventListener('click', () => {
+    selectedOption.textContent = option.dataset.value;
+    dropdown.classList.remove('show');
+  });
+});
+
+document.addEventListener('click', (e) => {
+  if (!dropdown.contains(e.target)) {
+    dropdown.classList.remove('show');
+  }
+});
